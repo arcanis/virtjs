de431f0d8cd8ae031eac4bfa8003e1129a48051d
Maël Nison <nison.mael@gmail.com>
2014-06-25 23:15:22 +0200

    Fixes angular example :)

--- examples/gb/angular/startup.js
+++ examples/gb/angular/startup.js
@@ -5,9 +5,10 @@ require( [
     'virtjs',
     'virtjs-gb'
 
-], function ( Virtjs ) {
+], function ( Virtjs, Gameboy ) {
 
     window.Virtjs = Virtjs;
+    window.Virtjs.engine.GameBoy = Gameboy;
 
     angular.element( document ).ready( function ( ) {
         angular.bootstrap( document, [ 'application' ] );
